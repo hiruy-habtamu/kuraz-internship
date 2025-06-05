31578870a540ee87d122a94fd1cb0837583e5568
Hiruy Habtamu <hiruyhabtamu12@gmail.com>
2025-06-05 14:42:32 +0300

    Adds fields on TaskSerializer to fix error

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,6 +4,7 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
+        fields = '__all__'
 
 class TaskListSerializer(serializers.ModelSerializer):
     class Meta:
